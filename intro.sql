9b1047faa79b3b3ede9b4215f8a1272f74688994
dundun <costes.duncan@gmail.com>
2025-03-19 00:23:08 +0100

    ajout du crud

---
+++ intro.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXIST Albums (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    price FLOAT NOT NULL
+);
+
+INSERT INTO Albums (title, artist, price)
+VALUES ('imagine', 'john lenon',19);
